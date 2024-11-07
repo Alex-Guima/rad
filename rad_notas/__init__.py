e2d8670dc5108062918244936b11b91d73821469
Alex-Guima <alexandreagtaveira@gmail.com>
2024-11-07 11:58:58 -0300

    Change github repository

--- rad_notas/__init__.py
+++ rad_notas/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'rad_notas'),
     )
 
     try:
--- rad_notas/db/avaliacao.py
+++ rad_notas/db/avaliacao.py
@@ -1,21 +1,35 @@
-from peewee import Field, Model
+from peewee import Field, AutoField
+from string import Template
+import psycopg
 
-
-class Avaliacao(Model):
-  id = AutoField()
-
-
+from rad_notas.db.base_model import BaseModel
 
 class PrefixedSerial(Field):
-  field_type = 'prefixed_serial'
+    field_type = 'text'
 
-  def db_value(self, value):
-    return
+    def get_count(self):
+        with psycopg.connect() as conn:
+            cur = conn.cursor()
+            query_entries = cur.execute("SELECT COUNT(*) FROM avaliacao").fetchone()
+        if query_entries == None:
+            entry_count = 0
+            return str(entry_count)
+        else:
+            entry_count = query_entries[0] + 1
+            return str(entry_count)
 
-  def python_value(self, value):
+    def db_value(self, value):
+        return AutoField()
 
-    if (value not in ["PROVA", "NF"]):
-      raise ValueError("Invalid evaluation method")
+    def python_value(self, value):
+        entry_count = self.get_count()
+        template_string = Template("$value_$entry_count")
+        if (value not in ["PROVA", "NF"]):
+            raise ValueError("Invalid evaluation method")
+        else:
+            concatenated_string = template_string.substitute(value, entry_count=str(entry_count))
+            return concatenated_string
 
-    field_string = value + str(Avaliacao.select().count() + 1)
-    return field_string
+
+class Avaliacao(BaseModel):
+    id = PrefixedSerial(null=False, primary_key=True)
